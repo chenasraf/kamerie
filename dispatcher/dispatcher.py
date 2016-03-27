56250583bc56deb7bd62071c2547c4d6add05be1
Dor Munis <dormunis@gmail.com>
2016-03-28 00:16:29 +0300

    Created basic dispatcher

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -1,25 +1,52 @@
 """
 Dispatcher API daemon
 """
+import threading
 import time
 import zmq
+from random import randint
+import logging
+
+KAMERIE_NAME = "kameried"
 
 DEFAULT_PORT = 5555
 
+KEEP_ALIVE_INTERVAL = 10  # secs
 
-class Dispatcher(object):
-    def __init__(self, port=DEFAULT_PORT):
+KEEP_ALIVE = b"keep_alive"
+TRUE_SIGNAL = b"true"
+CLOSE_SIGNAL = b"connection_closed"
+
+DEFAULT_THREAD_NAME = "default_thread_name"
+
+
+class Dispatcher(threading.Thread):
+    def __init__(self, thread_id=None, thread_name=DEFAULT_THREAD_NAME, port=DEFAULT_PORT):
         # Prepare context
-        context = zmq.Context()
-        publisher = context.socket(zmq.PUB)
-        publisher.bind("tcp://*:{port}".format(port=port))
+        super(Dispatcher, self).__init__()
+        self.alive = True
+        self.thread_id = thread_id if thread_id is not None else randint(0, 1000)
+        self.thread_name = thread_name
 
-        while True:
-            time.sleep(1)
+        logging.basicConfig(level=logging.DEBUG)
+        self._logger = logging.getLogger(__name__)
 
-        publisher.close()
-        context.term()
+        self.context = zmq.Context()
+        self.publisher = self.context.socket(zmq.PUB)
+        self.publisher.bind("tcp://*:{port}".format(port=port))
 
+    def run(self):
+        self._logger.info("Starting thread #{id}, name: {name}".format(id=self.thread_id, name=self.thread_name))
+        while self.alive:
+            self.keep_alive()
 
-if __name__ == '__main__':
-    Dispatcher()
+    def close(self):
+        self._logger.info("Exiting thread #{id}, name: {name}".format(id=self.thread_id, name=self.thread_name))
+        self.publisher.close()
+        self.context.term()
+        self.alive = False
+
+    def keep_alive(self):
+        self._logger.info("Sent keep alive signal")
+        self.publisher.send_multipart(msg_parts=[KEEP_ALIVE, TRUE_SIGNAL])
+        time.sleep(KEEP_ALIVE_INTERVAL)
--- tests/subscriber_test.py
+++ tests/subscriber_test.py
@@ -1,11 +1,38 @@
 import unittest
 
+import zmq
+
+from datetime import datetime, timedelta
+import time
+
+from dispatcher import dispatcher
+
+
 class SubscriberTest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.dispatcher = dispatcher.Dispatcher()
+        self.context = zmq.Context()
+        self.subscriber = self.context.socket(zmq.SUB)
+        self.subscriber.connect("tcp://localhost:{port}".format(port=dispatcher.DEFAULT_PORT))
+        self.dispatcher.start()
 
     def tearDown(self):
-        pass
+        self.subscriber.close()
+        self.context.term()
+        self.dispatcher.close()
 
-    def test(self):
-        pass
+    def test_keep_alive_interval_test(self):
+        time.sleep(1)
+        start_time = datetime.now()
+        self.subscriber.setsockopt(zmq.SUBSCRIBE, dispatcher.KEEP_ALIVE)
+        self.subscriber.recv_multipart()
+        self.assertGreaterEqual(timedelta(seconds=dispatcher.KEEP_ALIVE_INTERVAL),
+                                datetime.now() - start_time,
+                                "Keep alive arrived late. like your girlfriend")
+
+    def test_one_subscriber(self):
+        self.subscriber.setsockopt(zmq.SUBSCRIBE, dispatcher.KEEP_ALIVE)
+        [address, content] = self.subscriber.recv_multipart()
+
+        self.assertEqual(dispatcher.KEEP_ALIVE, address, "got msg from different event: %s" % address)
+        self.assertIsNotNone(content)
