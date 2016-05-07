b6436867f9aea7ab1dc62c20158ff5edf8a2291d
Chen Asraf <chen@asraf.me>
2016-05-08 00:12:29 +0300

    Small update

--- kamerie/dispatcher/dispatcher.py
+++ kamerie/dispatcher/dispatcher.py
@@ -14,10 +14,11 @@ from bson import json_util
 from media_scanner import MediaScanner, TYPE_MOVIE, TYPE_SERIES
 from template_plugin.consts import DISPATCHER_NAME, EXCHANGE_NAME
 
-PLUGIN_DIRECTORY = '/home/dor/dev/python/kamerie/kamerie-plugins/'
+PLUGIN_DIRECTORY = os.path.join(os.path.dirname(__file__), '../../../kamerie-plugins')
 
 
 class Dispatcher(object):
+
     def __init__(self):
         # Prepare instance
         self.name = DISPATCHER_NAME
@@ -35,6 +36,7 @@ class Dispatcher(object):
         # rabbitmq
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = self.connection.channel()
+        self._logger.info('Connected to RabbitMQ successfully')
 
         self.channel.exchange_declare(exchange=EXCHANGE_NAME, type='direct')
         self.on_message({'media_path': '/home/dor/Videos/movies', 'media_type': TYPE_MOVIE})
@@ -67,16 +69,13 @@ class Dispatcher(object):
         if isinstance(message, dict) and all(k in ['media_type', 'media_path'] for k in message.keys()):
             if not message.get('scanned', False):
                 for scanner_message in self.media_scanner.scan_directory(message['media_path'], message['media_type']):
-                    self.channel.basic_publish(exchange=EXCHANGE_NAME, routing_key='', body=json_util.dumps(scanner_message))
+                    self.channel.basic_publish(exchange=EXCHANGE_NAME, routing_key='',
+                                               body=json_util.dumps(scanner_message))
             else:
+                self._logger.info('Publishing message to all plugins: %s' % str(message))
                 self.channel.basic_publish(exchange=EXCHANGE_NAME, routing_key='', body=json.dumps(message))
-            # for plugin in self.plugins:
-            #     plugin['plugin_cls']._message_received(message)
         else:
             self._logger.error("Invalid message: %s" % str(message))
-
-    def send_message(self, message):
-        pass
 
     def __exit__(self):
         self.close()
